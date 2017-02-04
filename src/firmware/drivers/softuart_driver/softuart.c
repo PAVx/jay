29d3e2db0b7da04a9b0d3593de722f726620bae7
Niraj Raniga <niraj.xr@gmail.com>
2017-02-04 15:25:13 -0800

    Niraj/softuart2 (#3)
    * added queue implementation to tx buffer
    
    * moved software uart variables into struct
    
    * multiple softuarts, but glitchy when using more than one
    
    * Softuart working at a baud of 600. Added 64bit prescalar.
    
    * fixed for merge

--- src/firmware/drivers/softuart_driver/softuart.c
+++ src/firmware/drivers/softuart_driver/softuart.c
@@ -1,14 +1,6 @@
 // softuart.c
 // AVR-port of the generic software uart written in C
 //
-// Generic code from
-// Colin Gittins, Software Engineer, Halliburton Energy Services
-// (has been available from iar.com web-site -> application notes)
-//
-// Generic software uart written in C, requiring a timer set to 3 times
-// the baud rate, and two software read/write pins for the receive and
-// transmit functions.
-//
 // * Received characters are buffered
 // * putchar(), getchar(), kbhit() and flush_input_buffer() are available
 // * There is a facility for background processing while waiting for input
@@ -56,100 +48,177 @@
 #include <util/delay.h>
 
 #include "softuart.h"
+#include "Queue.h"
 
 #define SU_TRUE    1
 #define SU_FALSE   0
 
 // startbit and stopbit parsed internally (see ISR)
 #define RX_NUM_OF_BITS (8)
-static char           inbuf[SOFTUART_IN_BUF_SIZE];
-static unsigned char  qin;
-static unsigned char           qout;
-static unsigned char  flag_rx_off;
-static unsigned char  flag_rx_ready;
 
 // 1 Startbit, 8 Databits, 1 Stopbit = 10 Bits/Frame
 #define TX_NUM_OF_BITS (10)
-static unsigned char  flag_tx_busy;
-static unsigned char  timer_tx_ctr;
-static unsigned char  bits_left_in_tx;
-static unsigned short internal_tx_buffer; /* ! mt: was type uchar - this was wrong */
 
-#define set_tx_pin_high()      ( SOFTUART_TXPORT |=  ( 1 << SOFTUART_TXBIT ) )
-#define set_tx_pin_low()       ( SOFTUART_TXPORT &= ~( 1 << SOFTUART_TXBIT ) )
-#define get_rx_pin_status()    ( SOFTUART_RXPIN  &   ( 1 << SOFTUART_RXBIT ) )
+static softUART channel[SOFTUART_CHANNELS];
+
+void set_tx_pin_high(int i) {
+		#ifdef SOFTUART_TXPORT_1
+		if(i == 0) {
+			( SOFTUART_TXPORT_1 |=  ( 1 << SOFTUART_TXBIT_1 ) );
+		}
+		#endif
+
+		#ifdef SOFTUART_TXPORT_2
+		else if(i == 1){
+			( SOFTUART_TXPORT_2 |=  ( 1 << SOFTUART_TXBIT_2 ) );
+		}
+		#endif
+
+		#ifdef SOFTUART_TXPORT_3
+		else if(i == 2){
+			( SOFTUART_TXPORT_3 |=  ( 1 << SOFTUART_TXBIT_3 ) );
+		}
+		#endif
+
+		#ifdef SOFTUART_TXPORT_4
+		else if(i == 3){
+			( SOFTUART_TXPORT_4 |=  ( 1 << SOFTUART_TXBIT_4 ) );
+		}
+		#endif
+	}
+
+void set_tx_pin_low(int i) {
+	#ifdef SOFTUART_TXPORT_1
+	if(i == 0) {
+		( SOFTUART_TXPORT_1 &= ~( 1 << SOFTUART_TXBIT_1 ) );
+	}
+	#endif
+
+	#ifdef SOFTUART_TXPORT_2
+	else if(i == 1){
+		( SOFTUART_TXPORT_2 &= ~( 1 << SOFTUART_TXBIT_2 ) );
+	}
+	#endif
+
+	#ifdef SOFTUART_TXPORT_3
+	else if(i == 2){
+		( SOFTUART_TXPORT_3 &= ~( 1 << SOFTUART_TXBIT_3 ) );
+	}
+	#endif
+
+	#ifdef SOFTUART_TXPORT_4
+	else if(i == 3){
+		( SOFTUART_TXPORT_4 &= ~( 1 << SOFTUART_TXBIT_4 ) );
+	}
+	#endif
+}
+
+int get_rx_pin_status(int i) {
+	if(i == 0) {
+		return ( SOFTUART_RXPIN_1  &   ( 1 << SOFTUART_RXBIT_1 ) );
+	}
+
+	#ifdef SOFTUART_RXPIN_2
+	else if(i == 1){
+		return ( SOFTUART_RXPIN_2  &   ( 1 << SOFTUART_RXBIT_2 ) );
+	}
+	#endif
+
+	#ifdef SOFTUART_RXPIN_3
+	else if(i == 2){
+		return ( SOFTUART_RXPIN_3  &   ( 1 << SOFTUART_RXBIT_3 ) );
+	}
+	#endif
+
+	#ifdef SOFTUART_RXPIN_4
+	else if(i == 3){
+		return ( SOFTUART_RXPIN_4  &   ( 1 << SOFTUART_RXBIT_4 ) );
+	}
+	#endif
+
+	return 0;
+}
 
 ISR(SOFTUART_T_COMP_LABEL)
 {
-	static unsigned char flag_rx_waiting_for_stop_bit = SU_FALSE;
-	static unsigned char rx_mask;
+	//int i = 1;
+	for(int i = 0; i < SOFTUART_CHANNELS; i++){
+		channel[i].isr.flag_rx_waiting_for_stop_bit = SU_FALSE;
 
-	static unsigned char timer_rx_ctr;
-	static unsigned char bits_left_in_rx;
-	static unsigned char internal_rx_buffer;
+		unsigned char start_bit = '0', flag_in = '0';
+		unsigned char tmp = '0';
 
-	unsigned char start_bit, flag_in;
-	unsigned char tmp;
+		// Transmitter Section
+		if ( channel[i].tx.flag_tx_busy == SU_TRUE ) {
 
-	// Transmitter Section
-	if ( flag_tx_busy == SU_TRUE ) {
-		tmp = timer_tx_ctr;
-		if ( --tmp == 0 ) { // if ( --timer_tx_ctr <= 0 )
-			if ( internal_tx_buffer & 0x01 ) {
-				set_tx_pin_high();
+			if(channel[i].tx.flag_ok_to_pop == SU_TRUE){
+				channel[i].isr.tx_byte = getFront(channel[i].tx.tx_buffer);
+				Dequeue(channel[i].tx.tx_buffer);
+				channel[i].tx.flag_ok_to_pop = SU_FALSE;
 			}
-			else {
-				set_tx_pin_low();
-			}
-			internal_tx_buffer >>= 1;
-			tmp = 3; // timer_tx_ctr = 3;
-			if ( --bits_left_in_tx == 0 ) {
-				flag_tx_busy = SU_FALSE;
-			}
-		}
-		timer_tx_ctr = tmp;
-	}
 
-	// Receiver Section
-	if ( flag_rx_off == SU_FALSE ) {
-		if ( flag_rx_waiting_for_stop_bit ) {
-			if ( --timer_rx_ctr == 0 ) {
-				flag_rx_waiting_for_stop_bit = SU_FALSE;
-				flag_rx_ready = SU_FALSE;
-				inbuf[qin] = internal_rx_buffer;
-				if ( ++qin >= SOFTUART_IN_BUF_SIZE ) {
-					// overflow - reset inbuf-index
-					qin = 0;
+			tmp = channel[i].tx.timer_tx_ctr;
+			if ( --tmp == 0 ) { // if ( --timer_tx_ctr <= 0 )
+				if ( channel[i].isr.tx_byte & 0x01 ) {
+					set_tx_pin_high(i);
+				}
+				else {
+					set_tx_pin_low(i);
+				}
+
+				channel[i].isr.tx_byte >>= 1;
+				tmp = 3; // timer_tx_ctr = 3;
+				if ( --channel[i].tx.bits_left_in_tx == 0 ) {
+					channel[i].tx.flag_tx_busy = SU_FALSE;
+					channel[i].tx.flag_ok_to_pop = SU_TRUE;
+				} else if ( channel[i].tx.bits_left_in_tx%TX_NUM_OF_BITS == 0 ) {
+					channel[i].tx.flag_ok_to_pop = SU_TRUE;
 				}
 			}
+			channel[i].tx.timer_tx_ctr = tmp;
 		}
-		else {  // rx_test_busy
-			if ( flag_rx_ready == SU_FALSE ) {
-				start_bit = get_rx_pin_status();
-				// test for start bit
-				if ( start_bit == 0 ) {
-					flag_rx_ready      = SU_TRUE;
-					internal_rx_buffer = 0;
-					timer_rx_ctr       = 4;
-					bits_left_in_rx    = RX_NUM_OF_BITS;
-					rx_mask            = 1;
-				}
-			}
-			else {  // rx_busy
-				tmp = timer_rx_ctr;
-				if ( --tmp == 0 ) { // if ( --timer_rx_ctr == 0 ) {
-					// rcv
-					tmp = 3;
-					flag_in = get_rx_pin_status();
-					if ( flag_in ) {
-						internal_rx_buffer |= rx_mask;
-					}
-					rx_mask <<= 1;
-					if ( --bits_left_in_rx == 0 ) {
-						flag_rx_waiting_for_stop_bit = SU_TRUE;
+
+		// Receiver Section
+		if ( channel[i].rx.flag_rx_off == SU_FALSE ) {
+			if ( channel[i].isr.flag_rx_waiting_for_stop_bit ) {
+				if ( --channel[i].isr.timer_rx_ctr == 0 ) {
+					channel[i].isr.flag_rx_waiting_for_stop_bit = SU_FALSE;
+					channel[i].rx.flag_rx_ready = SU_FALSE;
+					channel[i].rx.inbuf[channel[i].rx.qin] = channel[i].isr.internal_rx_buffer;
+					if ( ++channel[i].rx.qin >= SOFTUART_IN_BUF_SIZE ) {
+						// overflow - reset inbuf-index
+						channel[i].rx.qin = 0;
 					}
 				}
-				timer_rx_ctr = tmp;
+			}
+			else {  // rx_test_busy
+				if ( channel[i].rx.flag_rx_ready == SU_FALSE ) {
+					start_bit = get_rx_pin_status(i);
+					// test for start bit
+					if ( start_bit == 0 ) {
+						channel[i].rx.flag_rx_ready      = SU_TRUE;
+						channel[i].isr.internal_rx_buffer = 0;
+						channel[i].isr.timer_rx_ctr       = 4;
+						channel[i].isr.bits_left_in_rx    = RX_NUM_OF_BITS;
+						channel[i].isr.rx_mask            = 1;
+					}
+				}
+				else {  // rx_busy
+					tmp = channel[i].isr.timer_rx_ctr;
+					if ( --tmp == 0 ) { // if ( --timer_rx_ctr == 0 ) {
+						// rcv
+						tmp = 3;
+						flag_in = get_rx_pin_status(i);
+						if ( flag_in ) {
+							channel[i].isr.internal_rx_buffer |= channel[i].isr.rx_mask;
+						}
+						channel[i].isr.rx_mask <<= 1;
+						if ( --channel[i].isr.bits_left_in_rx == 0 ) {
+							channel[i].isr.flag_rx_waiting_for_stop_bit = SU_TRUE;
+						}
+					}
+					channel[i].isr.timer_rx_ctr = tmp;
+				}
 			}
 		}
 	}
@@ -157,10 +226,41 @@ ISR(SOFTUART_T_COMP_LABEL)
 
 static void io_init(void)
 {
+	/****************************************************************************/
+	// These are the initialization for Software UART Channel 1
+	#if SOFTUART_CHANNELS > 0
 	// TX-Pin as output
-	SOFTUART_TXDDR |=  ( 1 << SOFTUART_TXBIT );
+	SOFTUART_TXDDR_1 |= ( 1 << SOFTUART_TXBIT_1 );
 	// RX-Pin as input
-	SOFTUART_RXDDR &= ~( 1 << SOFTUART_RXBIT );
+	SOFTUART_RXDDR_1 &= ~( 1 << SOFTUART_RXBIT_1 );
+	#endif
+
+	/****************************************************************************/
+	// These are the initialization for Software UART Channel 2
+	#if SOFTUART_CHANNELS > 1
+	// TX-Pin as output
+	SOFTUART_TXDDR_2 |= ( 1 << SOFTUART_TXBIT_2 );
+	// RX-Pin as input
+	SOFTUART_RXDDR_2 &= ~( 1 << SOFTUART_RXBIT_2 );
+	#endif
+
+	/****************************************************************************/
+	// These are the initialization for Software UART Channel 3
+	#if SOFTUART_CHANNELS > 2
+	// TX-Pin as output
+	SOFTUART_TXDDR_3 |= ( 1 << SOFTUART_TXBIT_3 );
+	// RX-Pin as input
+	SOFTUART_RXDDR_3 &= ~( 1 << SOFTUART_RXBIT_3 );
+	#endif
+
+	/****************************************************************************/
+	// These are the initialization for Software UART Channel 4
+	#if SOFTUART_CHANNELS > 3
+	// TX-Pin as output
+	SOFTUART_TXDDR_4 |= ( 1 << SOFTUART_TXBIT_4 );
+	// RX-Pin as input
+	SOFTUART_RXDDR_4 &= ~( 1 << SOFTUART_RXBIT_4 );
+	#endif
 }
 
 static void timer_init(void)
@@ -184,11 +284,14 @@ static void timer_init(void)
 
 void softuart_init( void )
 {
-	flag_tx_busy  = SU_FALSE;
-	flag_rx_ready = SU_FALSE;
-	flag_rx_off   = SU_FALSE;
-
-	set_tx_pin_high(); /* mt: set to high to avoid garbage on init */
+	for(int i = 0; i < SOFTUART_CHANNELS; i++){
+		channel[i].tx.flag_tx_busy  = SU_FALSE;
+		channel[i].rx.flag_rx_ready = SU_FALSE;
+		channel[i].rx.flag_rx_off   = SU_FALSE;
+		channel[i].tx.flag_ok_to_pop = SU_TRUE;
+		channel[i].tx.tx_buffer = newQueue();
+		set_tx_pin_high(i); /* mt: set to high to avoid garbage on init */
+	}
 
 	io_init();
 	timer_init();
@@ -201,75 +304,76 @@ static void idle(void)
 	// add watchdog-reset here if needed
 }
 
-void softuart_turn_rx_on( void )
+void softuart_turn_rx_on( int i )
 {
-	flag_rx_off = SU_FALSE;
+	channel[i].rx.flag_rx_off = SU_FALSE;
 }
 
-void softuart_turn_rx_off( void )
+void softuart_turn_rx_off( int i )
 {
-	flag_rx_off = SU_TRUE;
+	channel[i].rx.flag_rx_off = SU_TRUE;
 }
 
-char softuart_getchar( void )
+char softuart_getchar( int i )
 {
 	char ch;
 
-	while ( qout == qin ) {
+	while ( channel[i].rx.qout == channel[i].rx.qin ) {
 		idle();
 	}
-	ch = inbuf[qout];
-	if ( ++qout >= SOFTUART_IN_BUF_SIZE ) {
-		qout = 0;
+	ch = channel[i].rx.inbuf[channel[i].rx.qout];
+	if ( ++channel[i].rx.qout >= SOFTUART_IN_BUF_SIZE ) {
+		channel[i].rx.qout = 0;
 	}
 
 	return( ch );
 }
 
-unsigned char softuart_kbhit( void )
+unsigned char softuart_kbhit( int i )
 {
-	return( qin != qout );
+	return( channel[i].rx.qin != channel[i].rx.qout );
 }
 
-void softuart_flush_input_buffer( void )
+void softuart_flush_input_buffer( int i )
 {
-	qin  = 0;
-	qout = 0;
+	channel[i].rx.qin  = 0;
+	channel[i].rx.qout = 0;
 }
 
-unsigned char softuart_transmit_busy( void )
+unsigned char softuart_transmit_busy( int i )
 {
-	return ( flag_tx_busy == SU_TRUE ) ? 1 : 0;
+	return ( channel[i].tx.flag_tx_busy == SU_TRUE ) ? 1 : 0;
 }
 
-void softuart_putchar( const char ch )
+void softuart_putchar( const char ch , int i)
 {
-	while ( flag_tx_busy == SU_TRUE ) {
+	while ( getLength(channel[i].tx.tx_buffer) >= SOFTUART_OUT_BUF_SIZE-1) {
 		; // wait for transmitter ready
 		  // add watchdog-reset here if needed;
 	}
 
 	// invoke_UART_transmit
-	timer_tx_ctr       = 3;
-	bits_left_in_tx    = TX_NUM_OF_BITS;
-	internal_tx_buffer = ( ch << 1 ) | 0x200;
-	flag_tx_busy       = SU_TRUE;
+	channel[i].tx.timer_tx_ctr       = 3;
+	channel[i].tx.bits_left_in_tx    += TX_NUM_OF_BITS;			// V2: add number of bits to total needed to empty
+	channel[i].tx.internal_tx_buffer = ( ch << 1 ) | 0x200;
+	channel[i].tx.flag_tx_busy       = SU_TRUE;
+
+	Enqueue(channel[i].tx.tx_buffer, (int)channel[i].tx.internal_tx_buffer);								// V2: added a queue as a buffer.
 }
 
-void softuart_puts( const char *s )
+void softuart_puts( const char *s , int i)
 {
 	while ( *s ) {
-		softuart_putchar( *s );
+		softuart_putchar( *s , i);
 		s++;
-		_delay_us(113);
 	}
 }
 
-void softuart_puts_p( const char *prg_s )
+void softuart_puts_p( const char *prg_s , int i)
 {
 	char c;
 
 	while ( ( c = pgm_read_byte( prg_s++ ) ) ) {
-		softuart_putchar(c);
+		softuart_putchar(c, i);
 	}
 }
--- src/firmware/drivers/softuart_driver/softuart.h
+++ src/firmware/drivers/softuart_driver/softuart.h
@@ -1,86 +1,91 @@
+#include "Queue.h"
+
 #if !defined(F_CPU)
     #warning "F_CPU not defined in makefile - now defined in softuart.h"
     #define F_CPU 16000000UL
 
 #endif
 
-#define SOFTUART_BAUD_RATE      38400
+#define SOFTUART_BAUD_RATE      600
+#define SOFTUART_CHANNELS       2
 
-#if defined (__AVR_ATtiny25__) || defined (__AVR_ATtiny45__) || defined (__AVR_ATtiny85__)
-    #define SOFTUART_RXPIN   PINB
-    #define SOFTUART_RXDDR   DDRB
-    #define SOFTUART_RXBIT   PB0
+/****************************************************************************/
+// These are the definitions for Software UART Channel 1
+#if SOFTUART_CHANNELS > 0
+#define SOFTUART_RXPIN_1      PIND
+#define SOFTUART_RXDDR_1      DDRD
+#define SOFTUART_RXBIT_1      PD2
 
-    #define SOFTUART_TXPORT  PORTB
-    #define SOFTUART_TXDDR   DDRB
-    #define SOFTUART_TXBIT   PB1
-
-    #define SOFTUART_T_COMP_LABEL      TIM0_COMPA_vect
-    #define SOFTUART_T_COMP_REG        OCR0A
-    #define SOFTUART_T_CONTR_REGA      TCCR0A
-    #define SOFTUART_T_CONTR_REGB      TCCR0B
-    #define SOFTUART_T_CNT_REG         TCNT0
-    #define SOFTUART_T_INTCTL_REG      TIMSK
-
-    #define SOFTUART_CMPINT_EN_MASK    (1 << OCIE0A)
-
-    #define SOFTUART_CTC_MASKA         (1 << WGM01)
-    #define SOFTUART_CTC_MASKB         (0)
-
-    /* "A timer interrupt must be set to interrupt at three times
-       the required baud rate." */
-    #define SOFTUART_PRESCALE (8)
-    // #define SOFTUART_PRESCALE (1)
-
-    #if (SOFTUART_PRESCALE == 8)
-        #define SOFTUART_PRESC_MASKA         (0)
-        #define SOFTUART_PRESC_MASKB         (1 << CS01)
-    #elif (SOFTUART_PRESCALE==1)
-        #define SOFTUART_PRESC_MASKA         (0)
-        #define SOFTUART_PRESC_MASKB         (1 << CS00)
-    #else
-        #error "prescale unsupported"
-    #endif
-#elif defined (__AVR_ATmega324P__) || defined (__AVR_ATmega324A__)  \
-   || defined (__AVR_ATmega644P__) || defined (__AVR_ATmega644PA__) \
-   || defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328PA__) \
-   || defined (__AVR_ATmega164P__) || defined (__AVR_ATmega164A__)
-
-    #define SOFTUART_RXPIN   PIND
-    #define SOFTUART_RXDDR   DDRD
-    #define SOFTUART_RXBIT   PD2
-
-    #define SOFTUART_TXPORT  PORTD
-    #define SOFTUART_TXDDR   DDRD
-    #define SOFTUART_TXBIT   PD3
-
-    #define SOFTUART_T_COMP_LABEL      TIMER0_COMPA_vect
-    #define SOFTUART_T_COMP_REG        OCR0A
-    #define SOFTUART_T_CONTR_REGA      TCCR0A
-    #define SOFTUART_T_CONTR_REGB      TCCR0B
-    #define SOFTUART_T_CNT_REG         TCNT0
-    #define SOFTUART_T_INTCTL_REG      TIMSK0
-    #define SOFTUART_CMPINT_EN_MASK    (1 << OCIE0A)
-    #define SOFTUART_CTC_MASKA         (1 << WGM01)
-    #define SOFTUART_CTC_MASKB         (0)
-
-    /* "A timer interrupt must be set to interrupt at three times
-       the required baud rate." */
-    #define SOFTUART_PRESCALE (8)
-    // #define SOFTUART_PRESCALE (1)
-
-    #if (SOFTUART_PRESCALE == 8)
-        #define SOFTUART_PRESC_MASKA         (0)
-        #define SOFTUART_PRESC_MASKB         (1 << CS01)
-    #elif (SOFTUART_PRESCALE==1)
-        #define SOFTUART_PRESC_MASKA         (0)
-        #define SOFTUART_PRESC_MASKB         (1 << CS00)
-    #else
-        #error "prescale unsupported"
-    #endif
-#else
-    #error "no defintions available for this AVR"
+#define SOFTUART_TXPORT_1     PORTD
+#define SOFTUART_TXDDR_1      DDRD
+#define SOFTUART_TXBIT_1      PD3
 #endif
+
+/****************************************************************************/
+// These are the definitions for Software UART Channel 2
+#if SOFTUART_CHANNELS > 1
+#define SOFTUART_RXPIN_2      PIND
+#define SOFTUART_RXDDR_2      DDRD
+#define SOFTUART_RXBIT_2      PD4
+
+#define SOFTUART_TXPORT_2     PORTD
+#define SOFTUART_TXDDR_2      DDRD
+#define SOFTUART_TXBIT_2      PD5
+#endif
+
+/****************************************************************************/
+// These are the definitions for Software UART Channel 3
+#if SOFTUART_CHANNELS > 2
+#define SOFTUART_RXPIN_3      PIND
+#define SOFTUART_RXDDR_3      DDRD
+#define SOFTUART_RXBIT_3      PD6
+
+#define SOFTUART_TXPORT_3     PORTD
+#define SOFTUART_TXDDR_3      DDRD
+#define SOFTUART_TXBIT_3      PD7
+#endif
+
+/****************************************************************************/
+// These are the definitions for Software UART Channel 4
+#if SOFTUART_CHANNELS > 3
+#define SOFTUART_RXPIN_4      PIND
+#define SOFTUART_RXDDR_4      DDRD
+#define SOFTUART_RXBIT_4      PD8
+
+#define SOFTUART_TXPORT_4     PORTD
+#define SOFTUART_TXDDR_4      DDRD
+#define SOFTUART_TXBIT_4      PD9
+#endif
+
+
+#define SOFTUART_T_COMP_LABEL      TIMER0_COMPA_vect
+#define SOFTUART_T_COMP_REG        OCR0A
+#define SOFTUART_T_CONTR_REGA      TCCR0A
+#define SOFTUART_T_CONTR_REGB      TCCR0B
+#define SOFTUART_T_CNT_REG         TCNT0
+#define SOFTUART_T_INTCTL_REG      TIMSK0
+#define SOFTUART_CMPINT_EN_MASK    (1 << OCIE0A)
+#define SOFTUART_CTC_MASKA         (1 << WGM01)
+#define SOFTUART_CTC_MASKB         (0)
+
+/* "A timer interrupt must be set to interrupt at three times
+   the required baud rate." */
+#define SOFTUART_PRESCALE (64)
+// #define SOFTUART_PRESCALE (1)
+
+#if (SOFTUART_PRESCALE == 8)
+    #define SOFTUART_PRESC_MASKA         (0)
+    #define SOFTUART_PRESC_MASKB         (1 << CS01)
+#elif (SOFTUART_PRESCALE==1)
+    #define SOFTUART_PRESC_MASKA         (0)
+    #define SOFTUART_PRESC_MASKB         (1 << CS00)
+#elif (SOFTUART_PRESCALE==64)
+    #define SOFTUART_PRESC_MASKA         (0)
+    #define SOFTUART_PRESC_MASKB         (1 << CS01)|(1 << CS00)
+#else
+    #error "prescale unsupported"
+#endif
+
 
 #define SOFTUART_TIMERTOP ( F_CPU/SOFTUART_PRESCALE/SOFTUART_BAUD_RATE/3 - 1)
 
@@ -89,37 +94,75 @@
 #endif
 
 #define SOFTUART_IN_BUF_SIZE     32
+#define SOFTUART_OUT_BUF_SIZE    8
+
+typedef struct softuartRX_t{
+  char           inbuf[SOFTUART_IN_BUF_SIZE];
+  unsigned char  qin;
+  unsigned char  qout;
+  unsigned char  flag_rx_off;
+  unsigned char  flag_rx_ready;
+} softuartRX;
+
+typedef struct softuartTX_t{
+  unsigned char  flag_tx_busy;
+  unsigned char  timer_tx_ctr;
+  unsigned char  bits_left_in_tx;
+  unsigned short internal_tx_buffer; /* ! mt: was type uchar - this was wrong */
+
+  unsigned char flag_ok_to_pop;
+  Queue tx_buffer;
+} softuartTX;
+
+typedef struct softuartISR_t{
+  unsigned char flag_rx_waiting_for_stop_bit; // = SU_FALSE;
+  unsigned char rx_mask;
+
+  unsigned char timer_rx_ctr;
+  unsigned char bits_left_in_rx;
+  unsigned char internal_rx_buffer;
+  int tx_byte;
+} softuartISR;
+
+typedef struct softUART_t{
+  softuartRX rx;
+  softuartTX tx;
+  softuartISR isr;
+  uint32_t baud;
+  uint8_t type;
+  uint8_t id;
+} softUART;
 
 // Init the Software Uart
 void softuart_init(void);
 
 // Clears the contents of the input buffer.
-void softuart_flush_input_buffer( void );
+void softuart_flush_input_buffer( int i ) ;
 
 // Tests whether an input character has been received.
-unsigned char softuart_kbhit( void );
+unsigned char softuart_kbhit( int i );
 
 // Reads a character from the input buffer, waiting if necessary.
-char softuart_getchar( void );
+char softuart_getchar( int i );
 
 // To check if transmitter is busy
-unsigned char softuart_transmit_busy( void );
+unsigned char softuart_transmit_busy( int i );
 
 // Writes a character to the serial port.
-void softuart_putchar( const char );
+void softuart_putchar( const char, int i );
 
 // Turns on the receive function.
-void softuart_turn_rx_on( void );
+void softuart_turn_rx_on( int i );
 
 // Turns off the receive function.
-void softuart_turn_rx_off( void );
+void softuart_turn_rx_off( int i );
 
 // Write a NULL-terminated string from RAM to the serial port
-void softuart_puts( const char *s );
+void softuart_puts( const char *s, int i );
 
 // Write a NULL-terminated string from program-space (flash)
 // to the serial port. example: softuart_puts_p(PSTR("test"))
-void softuart_puts_p( const char *prg_s );
+void softuart_puts_p( const char *prg_s, int i );
 
 // Helper-Macro - "automatically" inserts PSTR
 // when used: include avr/pgmspace.h before this include-file
