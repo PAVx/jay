52117f1c17de506a23c07b30e02a214cf06af5a0
Sargis Yonan <sargisy@gmail.com>
2017-02-05 00:22:13 -0800

    added motor protocols and pins / working PWM

--- src/firmware/drivers/pwm/pwm_driver.c
+++ src/firmware/drivers/pwm/pwm_driver.c
@@ -1,9 +1,3 @@
-/*
- * pwm.c
- *
- *  Created on: Oct 13, 2014
- *      Author: jcobb
- */
 #include <avr/pgmspace.h>
 #include <stdbool.h>
 #include <avr/interrupt.h>
@@ -13,116 +7,52 @@
 
 #define LARGEST_8BIT_NUMBER     255
 #define LARGEST_16BIT_NUMBER    65535
-#define PWM_DEFAULT             32
+#define PWM_DEFAULT             0
 
-// TIME = PERIOD/FREQUENCY
-// 0.000032 = 256/8,000,000
-
+static volatile uint8_t duty_cycle_channel1 = PWM_DEFAULT;
 static volatile uint8_t duty_cycle_channel2 = PWM_DEFAULT;
 static volatile uint8_t duty_cycle_channel3 = PWM_DEFAULT;
 static volatile uint8_t duty_cycle_channel4 = PWM_DEFAULT;
-static volatile uint8_t duty_cycle_channel5 = PWM_DEFAULT;
-
-// Hadware PMW
-// Since we are using TCCR0A and TCCR0B in clock they need to be commented
-// Another option is to use soft_pwm
-
 
 void pwm_init()
 {
-
-    // setup our terminal callback
-
     DDRD |= (1<<DDD3);
     DDRB |= (1<<DDB1);
     DDRB |= (1<<DDB2);
     DDRB |= (1<<DDB3);
 
-
-    // Fast PWM overflow on REG A and REG B
-
-    // Commented due to clock
+    // prescalers
     TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10);
-    TCCR1B = _BV(CS11) | _BV(CS10);
+    TCCR1B = _BV(CS11);// | _BV(CS10);
     TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
-    TCCR2B = _BV(CS22);// | _BV(CS20);;
-
-    // Enable 3 Timers
-
-    // Commented due to clock
-    // enable timer1 interrupt
-    TIMSK1 |= _BV(TOIE1);
+    TCCR2B = _BV(CS21);// | _BV(CS20);;
 
     // Set interrupt enable
     sei();
 
     // Set initial duty cycle
-    OCR1A = duty_cycle_channel5;
-    OCR1B = duty_cycle_channel4;
-    OCR2A = duty_cycle_channel3;
-    OCR2B = duty_cycle_channel2;
+    OCR1A = duty_cycle_channel4;
+    OCR1B = duty_cycle_channel3;
+    OCR2A = duty_cycle_channel2;
+    OCR2B = duty_cycle_channel1;
 }
 
 void pwm_setval(uint8_t val, uint8_t channel)
 {
-    if (channel == 2) {
+    if (channel == 1) {
+        duty_cycle_channel1 = val;
+        OCR2B = duty_cycle_channel1;
+    } else if (channel == 2) {
         duty_cycle_channel2 = val;
+        OCR2A = duty_cycle_channel2;
     } else if (channel == 3) {
-            duty_cycle_channel3 = val;
+        duty_cycle_channel3 = val;
+        OCR1B = duty_cycle_channel3;
     } else if (channel == 4) {
-            duty_cycle_channel4 = val;
-    } else if (channel == 5) {
-            duty_cycle_channel5 = val;
+        duty_cycle_channel4 = val;
+        OCR1A = duty_cycle_channel4;
     }
 }
-
-volatile uint16_t frequency_counter = 0;
-
-//#define UPDATE_FREQUENCY  500 // 500Hz for motor
-#define UPDATE_FREQUENCY    50  // 50Hz for servo
-#define FACTOR              32  // 8000000/256
-/*
-#define FACTOR      8
-#define FACTOR      4
-#define FACTOR      1
-*/
-
-ISR(TIMER1_OVF_vect)
-{
-    frequency_counter++;
-
-    if(frequency_counter==(FACTOR*1000/UPDATE_FREQUENCY))
-    {
-        frequency_counter = 0;
-        OCR1A = duty_cycle_channel5;
-        OCR1B = duty_cycle_channel4;
-        OCR2A = duty_cycle_channel3;
-        OCR2B = duty_cycle_channel2;
-    }
-
-}
-
-/*
-int8_t pwm_sinus_array[256];
-for(int i=0; i<10; i++)
-{
-    pwm_sinus_array[i] = sin(2.0 * i / 10 * 3.14159265) * 127;
-}
-*/
-
-/*
-void pwm_init()
-{
-    OCR0A = 128;
-    TCCR0A |= _BV(COM0A1);
-    TCCR0A |= _BV(WGM01) | _BV(WGM00);
-    TCCR0B |= _BV(CS01);
-}
-ISR(PCINT0_vect)
-{
-}
-*/
-
 
 
 /*
--- src/firmware/drivers/pwm/pwm_driver.h
+++ src/firmware/drivers/pwm/pwm_driver.h
@@ -1,16 +1,7 @@
-/*
- * pwm.h
- *
- *  Created on: Oct 13, 2014
- *      Author: jcobb
- */
+#ifndef _PWM_H_
+#define _PWM_H_
 
-#ifndef PWM_H_
-#define PWM_H_
-
-void pwm_terminal_in_cb(uint8_t c);
-void pwm_init();
-void pwm_tick();
+void pwm_init(void);
 void pwm_setval(uint8_t val, uint8_t channel);
 
-#endif /* PWM_H_ */
+#endif /* _PWM_H_ */
