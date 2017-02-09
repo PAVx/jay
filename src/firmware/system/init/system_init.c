8d34c635eaffc5ecd9042a6756ab3b06657e01d4
Sargis Yonan <sargisy@gmail.com>
2017-02-08 16:31:31 -0800

    integrated timer int based imu in system

--- src/firmware/system/init/system_init.c
+++ src/firmware/system/init/system_init.c
@@ -1,13 +1,6 @@
 #include "system_init.h"
 #include "system.h"
 
-//#define GYRO (1)
-#define COM (1)
-#define MOTORS (1)
-#define SYSTEM_TICK (1)
-#define GYRO (1)
-#define ACCEL (1)
-
 uint8_t system_initialize(void) {
 	// initialize protcols 
 	#ifdef GYRO
@@ -21,7 +14,7 @@ uint8_t system_initialize(void) {
 	#ifdef COM
 		#define UART (1)
 		InitializeUART(HW_UART_BAUD);
-		packet_init();
+	//	packet_init();
 	#endif
 
 	#ifdef MOTORS
@@ -40,6 +33,8 @@ uint8_t system_initialize(void) {
 	#ifdef SYSTEM_TICK
 		clock_init();
 	#endif
+
+	sei();
 
 	return TRUE;
 }
