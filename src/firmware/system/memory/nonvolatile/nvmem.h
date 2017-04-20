e0169f4043137f544feea2051597d47f50593dd0
Sargis Yonan <sargisy@gmail.com>
2017-04-19 21:24:55 -0700

    added nonvolatile memory block writing and reading to EEPROM -- use for GPS home coordinates, flight flag for GPS, and PID gains (#16)

---
+++ src/firmware/system/memory/nonvolatile/nvmem.h
@@ -0,0 +1,11 @@
+// eeprom.h
+// Written by Sargis Yonan
+// April 2017
+
+#include <avr/io.h>
+#include <avr/eeprom.h>
+
+// returns address of block memory written to EEPROM
+unsigned int nvmem_write_block(void *data_ptr, uint32_t n_bytes);
+
+void nvmem_read_block(unsigned int addr, void *data_ptr, uint32_t n_bytes);
