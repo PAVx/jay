b4b11d387866997e3245d9a1631ce24e640192cb
Niraj Raniga <niraj@kamama.com>
2017-04-24 14:05:33 -0700

    rebased to origin/master, cleaned makefile

--- src/firmware/main.c
+++ src/firmware/main.c
@@ -45,64 +45,10 @@ int main (void) {
 			break;
 		}
 
-<<<<<<< 9ec24ab8a7228e3435029260bd1ad411cd249c87
-		if (1) { //if (system_ticked() == TRUE) {
-
-			#ifdef GYRO
-  			Gyro_Update();
-  		#endif
-
-  		#ifdef ACCEL
-  			Accel_Update();
-  		#endif
-
-        #ifdef GPS
-            NEO6M_GetChar();
-        #endif
-
-  		#ifdef LEDS
-  			//toggle_led(SYSTEM_LED);
-  		#endif
-
-		 	#ifdef GYRO
-				sprintf(gbuffer, "G_X = %f\nG_Y = %f\nG_Z = %f\n\n\r", Gyro_GetX(), Gyro_GetY(), Gyro_GetZ());
-				UART_SendString(gbuffer);
-				UART_SendString("\n");
-		 	#endif
-
-		 	#ifdef ACCEL
-  	 		    sprintf(abuffer, "A_X = %f\nA_Y = %f\nA_Z = %f\n\n\r", Accel_GetX(), Accel_GetY(), Accel_GetZ());
-		 		UART_SendString(abuffer);
-		 	#endif
-
-            #ifdef GPS
-
-                if(GPS_NewDataReady()) {
-
-                    memset(gpsbuffer, '\0', 50);
-                    GPS_UpdateData();
-                    struct tm time = GPS_GetTime();
-                    sprintf(gpsbuffer, "T: %02d:%02d:%02d\nL: %.0f\nL: %.0f\nS: %.2f\nA: %.2f\n", time.tm_hour, time.tm_min, time.tm_sec, GPS_GetLatitude(), GPS_GetLongitude(), GPS_GetSpeed(), GPS_GetAltitude());
-                    int i = 0;
-                    while (gpsbuffer[i] != '\0') {
-                        _uart_driver_SendByte(gpsbuffer[i]);
-                        i++;
-                    }
-                }
-
-            #endif
-
-			#ifdef UART
-  		 		//UART_SendString("\nHW_UART PRINT\n\r");
-  		#endif
-
-			#ifdef SW_UART
-		 		//softuart_puts("S\n",0);
-=======
 		if (system_ticked() == TRUE) {
 			#ifdef CAM
 				Cam_Update();
->>>>>>> progress on PID loop
+
 			#endif
 			Gyro_Update();
 			Accel_Update();
@@ -137,16 +83,5 @@ int main (void) {
   			toggle_led(SYSTEM_LED);
 			system_untick();
 		}
-
-<<<<<<< 9ec24ab8a7228e3435029260bd1ad411cd249c87
-		//softuart_puts("sup\n",0);
-		//UART_SendString("\nHW_UART PRINT\n\r");
-
-		// todo:
-		// update accel registers
-		// update gps registers
-		// update PID controller for stabality
-=======
->>>>>>> progress on PID loop
 	}
 }
